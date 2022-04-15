76093104fa9cfe346b0221f77b2c6b4d8f71bdaf
kalebishop <kaleb.bishop@colorado.edu>
2022-04-15 16:18:36 -0600

    temporarily removed unused audio dependencies

--- src/misty_wrapper/simple_av_client.py
+++ src/misty_wrapper/simple_av_client.py
@@ -39,53 +39,53 @@ class VidStreamer:
         self.stopped = True
 
 
-import av
-# import numpy as np
-# from threading import Thread
-import pyaudio
-from collections import deque
-import time
+# import av
+# # import numpy as np
+# # from threading import Thread
+# import pyaudio
+# from collections import deque
+# import time
 
 
-class AudioPlayer:
+# class AudioPlayer:
 
-    def __init__(self, path):
-        self.stream_path = path
+#     def __init__(self, path):
+#         self.stream_path = path
 
 
-    def update(self):
-        queue = deque()
+#     def update(self):
+#         queue = deque()
 
-        def play_thread():
-            p = pyaudio.PyAudio()
-            stream = p.open(format=pyaudio.paFloat32,
-                            channels=1,
-                            rate=11025,
-                            output=True)
-            while True:
-                if len(queue) == 0:
-                    time.sleep(0.25)
-                    continue
-                frame = queue.popleft()
-                stream.write(frame.to_ndarray().astype(np.float32).tostring())
+#         def play_thread():
+#             p = pyaudio.PyAudio()
+#             stream = p.open(format=pyaudio.paFloat32,
+#                             channels=1,
+#                             rate=11025,
+#                             output=True)
+#             while True:
+#                 if len(queue) == 0:
+#                     time.sleep(0.25)
+#                     continue
+#                 frame = queue.popleft()
+#                 stream.write(frame.to_ndarray().astype(np.float32).tostring())
 
-        t = Thread(target=play_thread)
-        t.start()  
+#         t = Thread(target=play_thread)
+#         t.start()  
 
-        container = av.open(self.stream_path)
-        input_stream = container.streams.get(audio=0)[0]
-        for frame in container.decode(input_stream):
-            frame.pts = None
-            queue.append(frame)
+#         container = av.open(self.stream_path)
+#         input_stream = container.streams.get(audio=0)[0]
+#         for frame in container.decode(input_stream):
+#             frame.pts = None
+#             queue.append(frame)
 
-    def start(self):
+#     def start(self):
 
-        t = Thread(target=self.update, args=())
-        t.daemon = True
-        t.start()
+#         t = Thread(target=self.update, args=())
+#         t.daemon = True
+#         t.start()
 
-    # def stop(self):
-    #     self.stopped = True
+#     # def stop(self):
+#     #     self.stopped = True
 
 
 if __name__ == "__main__":
